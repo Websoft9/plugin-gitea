899b7584d0fcac014940a8964ffa00d105ba0900
zhaojing1987 <jing.zhao@websoft9.com>
2023-10-11 16:36:06 +0800

    update

---
+++ changelog_latest.md
@@ -0,0 +1,1 @@
+1. Fix some known bugs
---
+++ src/App.js
@@ -0,0 +1,89 @@
+import axios from 'axios';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import cockpit from 'cockpit';
+import ini from 'ini';
+import { useEffect, useState } from 'react';
+import { Alert } from 'react-bootstrap';
+import Spinner from 'react-bootstrap/Spinner';
+import './App.css';
+
+function App() {
+  const [iframeSrc, setIframeSrc] = useState(null);
+  const [iframeKey, setIframeKey] = useState(Math.random());
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  let protocol = window.location.protocol;
+  let host = window.location.host;
+  const baseURL = protocol + "//" + (/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(host) ? host.split(":")[0] : host);
+
+  async function autoLogin() {
+    try {
+      var userName;
+      var userPwd;
+      const content = await cockpit.file('/data/websoft9/appmanage_new/src/config/config.ini').read();
+      const config = ini.parse(content);
+      userName = config.gitea.user_name
+      userPwd = config.gitea.user_pwd
+
+      if (!userName || !userPwd) {
+        setShowAlert(true);
+        setAlertMessage("Gitea Username or Password is empty.");
+        return;
+      }
+
+      const response = await axios.get(baseURL + '/w9git/user/login');
+      const doc = new DOMParser().parseFromString(response.data, 'text/html');
+      const csrf = doc.querySelector('input[name="_csrf"]').value;
+
+      const payload = new URLSearchParams();
+      payload.append('_csrf', csrf);
+      payload.append('user_name', userName);
+      payload.append('password', userPwd);
+
+      const login_response = await axios.post(baseURL + "/w9git/user/login", payload.toString(),
+        {
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          withCredentials: true
+        });
+      if (login_response.status != 200) {
+        setShowAlert(true);
+        setAlertMessage("Gitea Login Error.");
+        return;
+      }
+      setIframeSrc(baseURL + '/w9git/explore/repos');
+    } catch (error) {
+      setShowAlert(true);
+      setAlertMessage("Gitea Login Error." + error);
+    }
+  }
+
+  useEffect(() => {
+    autoLogin();
+    return () => {
+    }
+  }, []);
+
+  return (
+    <>
+      {
+        iframeSrc ? (
+          <div className='myGitea' key='container'>
+            <iframe title='Gitea' src={iframeSrc} />
+          </div>
+        ) : (
+          <div className="d-flex align-items-center justify-content-center m-5" style={{ flexDirection: "column" }}>
+            <Spinner animation="border" variant="secondary" className='mb-5' />
+            {showAlert && <Alert variant="danger" className="my-2">
+              {alertMessage}
+            </Alert>}
+          </div>
+        )
+      }
+    </>
+  );
+}
+
+export default App;
